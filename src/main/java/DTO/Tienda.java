3aa12238de79da376fbc9db11cbe1577dca3cd05
brayanPV <brayanstivenpv@ufps.edu.co>
2020-05-28 06:40:57 -0500

    creacion del proyecto, dto y dao

---
+++ src/main/java/DTO/Tienda.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author stive
+ */
+@Entity
+@Table(name = "tienda")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Tienda.findAll", query = "SELECT t FROM Tienda t")
+    , @NamedQuery(name = "Tienda.findById", query = "SELECT t FROM Tienda t WHERE t.id = :id")
+    , @NamedQuery(name = "Tienda.findByNombre", query = "SELECT t FROM Tienda t WHERE t.nombre = :nombre")
+    , @NamedQuery(name = "Tienda.findByLema", query = "SELECT t FROM Tienda t WHERE t.lema = :lema")
+    , @NamedQuery(name = "Tienda.findByEmail", query = "SELECT t FROM Tienda t WHERE t.email = :email")
+    , @NamedQuery(name = "Tienda.findByClave", query = "SELECT t FROM Tienda t WHERE t.clave = :clave")
+    , @NamedQuery(name = "Tienda.findByPropietario", query = "SELECT t FROM Tienda t WHERE t.propietario = :propietario")
+    , @NamedQuery(name = "Tienda.findByFacebook", query = "SELECT t FROM Tienda t WHERE t.facebook = :facebook")
+    , @NamedQuery(name = "Tienda.findByWeb", query = "SELECT t FROM Tienda t WHERE t.web = :web")
+    , @NamedQuery(name = "Tienda.findByImagen", query = "SELECT t FROM Tienda t WHERE t.imagen = :imagen")})
+public class Tienda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "lema")
+    private String lema;
+    @Lob
+    @Size(max = 65535)
+    @Column(name = "descripcion")
+    private String descripcion;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "clave")
+    private String clave;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "propietario")
+    private String propietario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "facebook")
+    private String facebook;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "web")
+    private String web;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 500)
+    @Column(name = "imagen")
+    private String imagen;
+    @ManyToMany(mappedBy = "tiendaList")
+    private List<Cliente> clienteList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tienda")
+    private List<Servicio> servicioList;
+
+    public Tienda() {
+    }
+
+    public Tienda(Integer id) {
+        this.id = id;
+    }
+
+    public Tienda(Integer id, String nombre, String lema, String email, String clave, String propietario, String facebook, String web, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.lema = lema;
+        this.email = email;
+        this.clave = clave;
+        this.propietario = propietario;
+        this.facebook = facebook;
+        this.web = web;
+        this.imagen = imagen;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLema() {
+        return lema;
+    }
+
+    public void setLema(String lema) {
+        this.lema = lema;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @XmlTransient
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void setClienteList(List<Cliente> clienteList) {
+        this.clienteList = clienteList;
+    }
+
+    @XmlTransient
+    public List<Servicio> getServicioList() {
+        return servicioList;
+    }
+
+    public void setServicioList(List<Servicio> servicioList) {
+        this.servicioList = servicioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tienda)) {
+            return false;
+        }
+        Tienda other = (Tienda) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "DTO.Tienda[ id=" + id + " ]";
+    }
+    
+}
